943835e2f8c01d521b1b235ccef9ec538c02356b
yamato0126 <craft1038jp@gmail.com>
2022-06-24 14:07:15 +0900

    add meigen quiz option

--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 
 Golang学習用の自己満ツール
 
-### Random Meigen Command
+## Random Meigen Command
+ランダムな名言を返すコマンド
 - 使用したAPI: https://meigen.doodlenote.net/
 - 参考にしたコード: https://github.com/divrhino/dadjoke
+
+誰の名言か当てるクイズ機能を追加(2022/6/24)
+- 参考にしたコード: https://qiita.com/ohkawa/items/c5e703459a1fe5cb83d0
--- cmd/meigen.go
+++ cmd/meigen.go
@@ -8,8 +8,10 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
+	"math/rand"
 	"net/http"
 	"strings"
+	"time"
 
 	"github.com/spf13/cobra"
 )
@@ -18,24 +20,68 @@ import (
 var meigenCmd = &cobra.Command{
 	Use:   "meigen",
 	Short: "random meigen",
-	Long:  `This command gets a random meigen`,
+	Long:  `This command gets a random meigen.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		getRandomMeigen()
+		quizFlag, err := cmd.Flags().GetBool("quiz")
+		if err != nil {
+			log.Printf("Flags error. %v", err)
+		} else if quizFlag {
+			makeMeigenQuiz()
+		} else {
+			printMeigen()
+		}
 	},
 }
 
 func init() {
+	meigenCmd.Flags().BoolP("quiz", "q", false, "Quiz function")
 	rootCmd.AddCommand(meigenCmd)
 }
 
-func getRandomMeigen() {
+func makeMeigenQuiz() {
+	var authors [3]string
+	var choice [3]string = [3]string{"A", "B", "C"}
+	var input string
+	var ans string
+
+	statement, author := getRandomMeigen()
+	fmt.Println(statement)
+	fmt.Println("Who left this meigen?")
+	authors[0] = author
+	for i := 1; i < 3; i++ {
+		_, author := getRandomMeigen()
+		authors[i] = author
+	}
+	randomIndex := shaffleChoices(3)
+	for i := 0; i < 3; i++ {
+		fmt.Println(choice[i] + ": " + authors[randomIndex[i]])
+		if randomIndex[i] == 0 {
+			ans = choice[i]
+		}
+	}
+	fmt.Println("Your answer -> ")
+	fmt.Scan(&input)
+	if input == ans {
+		fmt.Println("Yes! You are correct.")
+	} else {
+		fmt.Println("No! you are wrong...")
+		fmt.Println("Correct answer is " + ans)
+	}
+}
+
+func printMeigen() {
+	statement, author := getRandomMeigen()
+	fmt.Println(statement)
+	fmt.Println("by " + author)
+}
+
+func getRandomMeigen() (string, string) {
 	url := "https://meigen.doodlenote.net/api/json.php?c=1"
 	responseBytes := getMeigenData(url)
 
 	response := string(responseBytes)
 	split := strings.Index(response, "auther")
-	fmt.Println(response[12 : split-3])
-	fmt.Println("by " + response[split+9:len(response)-3])
+	return response[12 : split-3], response[split+9 : len(response)-3]
 }
 
 func getMeigenData(baseAPI string) []byte {
@@ -60,4 +106,35 @@ func getMeigenData(baseAPI string) []byte {
 	}
 
 	return responseBytes
+}
+
+func allKeys(m map[int]bool) []int {
+	i := 0
+	result := make([]int, len(m))
+	for key, _ := range m {
+		result[i] = key
+		i++
+	}
+	return result
+}
+
+func pickup(min int, max int, num int) []int {
+	numRange := max - min
+
+	selected := make(map[int]bool)
+	rand.Seed(time.Now().UnixNano())
+	for counter := 0; counter < num; {
+		n := rand.Intn(numRange) + min
+		if !selected[n] {
+			selected[n] = true
+			counter++
+		}
+	}
+	keys := allKeys(selected)
+	return keys
+}
+
+func shaffleChoices(num int) []int {
+	results := pickup(0, num, num)
+	return results
 }
